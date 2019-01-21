32de18ab0e59db7cb916c70931b1452e9466b6dd
quinlan <2874737621@qq.com>
2019-01-21 14:24:12 +0800

    '4_commit'

---
+++ 123.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+int main ()
+{
+    std::cout<<"dfsf\n";
+    return 0;
+}
---
+++ 设计模式/委托+继承.cpp
@@ -0,0 +1,45 @@
+/*
+    对于设计及对于同一份数据,具有不同的表现形式,且同一份数据改变,不同的表现形式全改变,可以用到,这种设计模式
+    我们设计一个父类,表示他的形式,同时不同的形式可以再次基础上进行派生,同业我们知道,可以用父类的指针,指向子类,这样,当我们
+    需要一种表现,形式时,我们向,subject进行注册,这样数据发生改变时,调用其updata导致所有数据均改变.
+*/
+#include<iostream>
+#include<vector>
+class subject;
+class observe{
+    public:
+    virtual void updata (subject* sub,int m_view);
+};
+class sbuject{
+    public:
+    void attach (observe *ob)
+    {
+        m_view.push_back(ob);
+    }
+    void set_value (int va)
+    {
+        value=va;
+        notify();
+    }
+    void dete (observe *ob)
+    {
+        for(int i=0;i<m_view.size();i++)
+        {
+            if(m_view[i]==ob)
+            {
+                m_view.erase(m_view.begin()+i);
+                break;
+            }
+        }
+    }
+    void notify ()
+    {
+        for(int i=0;i<m_view.size();i++)
+        {
+            m_view[i]->updata(this,value);
+        }
+    }
+    private:
+    int value;//数据
+    vector<observe*> m_view;//注册容器;
+};
